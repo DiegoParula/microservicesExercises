8e9878619c57de7af79aaa584653258767d283be
Diego Parula <parula27@gmail.com>
2024-02-19 18:21:03 -0300

    config server

---
+++ Ejercicio1SpringCloudConfig/ConsumerService/ConsumerService/src/main/java/com/example/ConsumerService/ConsumerServiceApplication.java
@@ -0,0 +1,38 @@
+package com.example.ConsumerService;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerServiceApplication.class, args);
+	}
+
+}
+
+@RestController
+class AccountService {
+	@Value("${message}")
+	private String message;
+	@Value("${global-message}")
+	private String globalMessage;
+
+	@RequestMapping(method = RequestMethod.GET, path="service")
+	public Map<String,String> message() {
+		Map<String,String> response = new HashMap<>();
+		response.put("message", message);
+		response.put("global-message", globalMessage);
+		return response;
+	}
+}
